f48339fdf02da7c1b41e4329425102dff6824cc8
Everton Azevedo <everton-zero@hotmail.com>
2025-07-02 19:26:44 -0300

    Atividade 02-07

---
+++ 02-07-2025/exemplos.py
@@ -0,0 +1,13 @@
+# cont = 0
+
+# while cont < 5:
+#     print("Olá, mundo!")
+#     #  cont += 1
+
+palavra=""
+while palavra != "sair":
+    palavra = input("Digite uma palavra: ")
+    print(f"A palavra digitada foi: {palavra}")  #  print(f"A palavra
+    #  digitada foi: {palavra}")
+    
+print("Fim do programa")  #  print("Fim do programa")  #
---
+++ 02-07-2025/exercicio1.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite um numero: "))
+print("Contagem regressiva:")
+while numero != 0:
+    print(numero)
+    numero -=1
